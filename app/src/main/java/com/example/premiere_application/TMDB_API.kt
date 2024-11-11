7a1c719723fa5c9c7ab48157f2e04f11726e3d62
Medaminesagh1 <147838684+Medaminesagh1@users.noreply.github.com>
2024-11-11 21:43:26 +0100

    verif API

--- app/src/main/java/com/example/premiere_application/TMDB_API.kt
+++ app/src/main/java/com/example/premiere_application/TMDB_API.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface TMDB_API {
 
     @GET("trending/movie/week")
-    suspend  fun derniers_films(@Query("api_key") api_key: String): Films
+    suspend fun derniers_films(@Query("api_key") api_key: String): Films
 
     @GET("trending/tv/week")
     suspend fun dernieres_series(@Query("api_key") api_key: String): Series
