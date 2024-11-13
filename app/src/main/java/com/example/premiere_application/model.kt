e6374d2faf14797ba28dc9cf27a6da63fab1dc17
msaghrao <mohamed_amine.saghraoui@etud.univ-jfc.fr>
2024-11-13 14:52:46 +0100

    modèle en place

--- app/src/main/java/com/example/premiere_application/model.kt
+++ app/src/main/java/com/example/premiere_application/model.kt
@@ -266,3 +266,27 @@ data class Film(
     val vote_average: Double = 0.0,
     val vote_count: Int = 0
 )
+
+data class ExamResponse(
+    val page: Int = 0,
+    val results: List<Film> = listOf(),
+    val total_pages: Int = 0,
+    val total_results: Int = 0
+)
+data class Exam(
+    val adult: Boolean = false,
+    val backdrop_path: String = "",
+    val genre_ids: List<Int> = listOf(),
+    val id: Int = 0,
+    val media_type: String = "",
+    val original_language: String = "",
+    val original_title: String = "",
+    val overview: String = "",
+    val popularity: Double = 0.0,
+    val poster_path: String = "",
+    val release_date: String = "",
+    val title: String = "",
+    val video: Boolean = false,
+    val vote_average: Double = 0.0,
+    val vote_count: Int = 0
+)
